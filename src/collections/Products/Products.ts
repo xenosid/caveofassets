71309495d09d331fd702375b4415a8bbee29bd58
Siddhartha Pratim Kumar <120655581+xenosid@users.noreply.github.com>
2024-03-25 18:27:51 +0530

    RECENT
    updated changes

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,13 +1,78 @@
-import { BeforeChangeHook } from 'payload/dist/collections/config/types'
+import { AfterChangeHook, BeforeChangeHook } from 'payload/dist/collections/config/types'
 import { PRODUCT_CATEGORIES } from '../../config'
-import { CollectionConfig } from 'payload/types'
-import { Product } from '../../payload-types'
+import { Access, CollectionConfig } from 'payload/types'
+import { Product, User } from '../../payload-types'
 import { stripe } from '../../lib/stripe'
 
-const addUser: BeforeChangeHook<Product> = async ({req, data}) => {
+const addUser: BeforeChangeHook<Product> = async ({
+  req, 
+  data,
+}) => {
   const user = req.user
 
-  return {...data, user: user.id}
+  return { ...data, user: user.id }
+}
+
+const syncUser: AfterChangeHook<Product> = async ({
+  req,
+  doc,
+}) => {
+  const fullUser = await req.payload.findByID({
+    collection: 'users',
+    id: req.user.id,
+  })
+
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
+
+    const  allIDs = [
+      ...(products?.map((product) =>
+      typeof product === 'object' ? product.id : product
+    ) || []),
+    ]
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
+
+    const dataToUpdate = [...createdProductIDs, doc.id]
+
+    await req.payload.update({
+        collection: 'users',
+        id: fullUser.id,
+        data: {
+        products: dataToUpdate,
+      },
+    })
+  }
+}
+
+const isAdminOrHasAccess = 
+(): Access => 
+({req: { user: _user } }) => {
+  const user = _user as User | undefined
+
+  if (!user) return false
+  if (user.role === 'admin') return true
+
+  const userProductIDs = (user.products || []).reduce<
+    Array<string>
+  >((acc, product) => {
+    if(!product) return acc
+    if(typeof product === 'string') {
+      acc.push(product)
+    } else {
+      acc.push(product.id)
+    } 
+
+    return acc
+  }, []) 
+
+  return{
+    id: {
+      in: userProductIDs,
+    },
+  }
 }
 
 export const Products: CollectionConfig = {
@@ -15,16 +80,24 @@ export const Products: CollectionConfig = {
     admin: {
       useAsTitle: 'name',
     },
-    access: {},
+    access: {
+      read: isAdminOrHasAccess(),
+      update: isAdminOrHasAccess(),
+      delete: isAdminOrHasAccess(),
+    },
     hooks: {
-      beforeChange: [ addUser, async (args) => {
-        if(args.operation === 'create') {
 
+      afterChange: [syncUser],
+      beforeChange: [ 
+        addUser, 
+        async (args) => {
+          if(args.operation === 'create') {
           const data = args.data as Product
 
-          const createdProduct = await stripe.products.create({
-            name: data.name,
-            default_price_data:{
+          const createdProduct = 
+            await stripe.products.create({
+              name: data.name,
+              default_price_data: {
               currency: 'INR',
               unit_amount: Math.round(data.price * 100),
             },
@@ -41,9 +114,9 @@ export const Products: CollectionConfig = {
           const data = args.data as Product
 
           const updatedProduct = 
-          await stripe.products.update(data.stripeId!, {
-            name: data.name,
-            default_price: data.priceId!,
+            await stripe.products.update(data.stripeId!, {
+              name: data.name,
+              default_price: data.priceId!,
           })
 
           const updated: Product = {
@@ -66,7 +139,7 @@ export const Products: CollectionConfig = {
         hasMany: false,
         admin: {
           condition: () => false,
-        }
+        },
       },
       {
         name: 'name',
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -23,4 +24,46 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice)
+}
+export function constructMetadata({
+  title = 'CaveOfAssets - the marketplace for game developers',
+  description = 'CaveOfAssets is an open-source marketplace for high-quality digital game assets.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@sid',
+    },
+    icons,
+    metadataBase: new URL('https://caveofassets.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
